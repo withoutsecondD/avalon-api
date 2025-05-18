e918d8ae1969cfc11c5b36c1e0371ba5ed03d373
Diyar Zulyarov <marbiru15@gmail.com>
2025-05-18 21:45:59 +0500

    api: construct skeleton of api with main endpoints

--- api/api.py
+++ api/api.py
@@ -1,37 +1,48 @@
-from pathlib import Path
 import dotenv
 import os
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, File, Request, UploadFile
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from typing import List
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 from api.utils import construct_response
 
 
-app = FastAPI()
-
 # load env variables
 dotenv.load_dotenv(dotenv.find_dotenv(), override=True)
 TF_SERVING_BASE_URL = os.getenv("TF_SERVING_BASE_URL")
+FRONTEND_ADDRESS = os.getenv("FRONTEND_ADDR")
 MODEL_NAME=os.getenv("MODEL_NAME")
 
-class PredictionRequest(BaseModel):
-    instances: List[float]
+app = FastAPI()
+app.mount("/resources", StaticFiles(directory="../resources"), name="resources")
+
+# allow requests from frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_ADDRESS],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 
 @app.post('/predict')
-def predict(req_data:PredictionRequest):
-    payload = {
-        'instances': req_data.instances
-    }
+async def predict(image:UploadFile=File(...)):
+    image_bytes = await image.read()
 
-    resp = requests.post(f'{TF_SERVING_BASE_URL}/v1/models/{MODEL_NAME}:predict', json=payload)
-    resp.raise_for_status()
+    print(image_bytes)
+    if len(image_bytes) > 10 * 1024 * 1024:
+        return RequestValidationError('file too large')
 
-    predictions = resp.json()
-    return construct_response(predictions)
+    return construct_response(image_bytes[:100])
+
+
+@app.get('/results')
+def results():
+    return construct_response({'hello': 'world'})
 
 
 @app.exception_handler(Exception)
@@ -43,8 +54,9 @@ async def exception_handler(req:Request, exc:Exception):
 
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc: RequestValidationError):
+async def validation_exception_handler(req:Request, exc: RequestValidationError):
     message = "; ".join([f"{err['msg']} at {err['loc']}" for err in exc.errors()])
+
     return JSONResponse(
         status_code=400,
         content=construct_response(None, message)
